07e6f9e1c2baf8253836594deec5b60c1a850bb9
Yan Jhong Cheng <108399737+YJCheng1210@users.noreply.github.com>
2022-07-13 17:24:45 +0800

    Add files via upload

---
+++ Word_Occurrency_Count.py
@@ -0,0 +1,56 @@
+def clean_Text(txt):
+    txt = txt.replace("’s", '')
+    txt = txt.replace("'s", '')
+    txt = txt.replace(',', '')
+    txt = txt.replace(".", '')
+    txt = txt.replace('!', '')
+    txt = txt.replace(';', '')
+    txt = txt.replace('?', '')
+    txt = txt.replace('-', '')
+    txt = txt.replace('"', '')
+    txt = txt.replace("'", '')
+    txt = txt.replace('“', '')
+    txt = txt.replace('”', '')
+    txt = txt.replace('‘', '')
+    txt = txt.replace('’', '')
+    txt = txt.replace('\n', '')
+    return txt
+
+f = open('FOMC Statement June 2022.txt', 'r', encoding='UTF-8') 
+
+# read lines
+content = f.readlines()
+
+f.close
+
+contentLines = ''
+ 
+characers = []
+stat = {}
+
+# Interate every line.
+for raw in content:
+    raw = clean_Text(raw)        # clean up the line
+    raw = raw.upper()
+    tmp_list = raw.split(' ')    # split by space
+
+    if len(raw) == 0:
+        continue
+    contentLines = contentLines + raw
+
+    for x in range(0, len(tmp_list)):
+        # If the string is first showed, then put in the "character" list
+        if not tmp_list[x] in characers:
+            characers.append(tmp_list[x])
+        # If str is first showed, then put in the "stat" dictionary
+        if tmp_list[x] not in stat:
+            stat[tmp_list[x]] = 1
+        # If showed, then add 1
+        stat[tmp_list[x]] += 1
+
+str = input('Enter a word to search: ')
+str = str.strip(' ')
+try:
+    print(f'[{str}] shows {stat[str.upper()]} times.')
+except KeyError:
+    print(f'There is no [{str}] in this article.')
